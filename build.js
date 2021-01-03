23cb66ec35741a63a5d996d3d25760881aa181ad
Lars Löffler <lars.loeffler@infodesire.com>
2021-01-03 20:51:27 +0100

    rename tasks to targets

--- build.js
+++ build.js
@@ -2,75 +2,75 @@ exports.setup = function( project ) {
 
   project.name = "tsunit";
   project.description = "A unit test environment for typescript and javascript";
-  project.defaultTask = "build";
+  project.defaultTarget = "build";
 
-  project.tasks[ "build" ] = {
+  project.targets[ "build" ] = {
     description: "Build the project, create distribution files.",
     depends: [ "test" ],
-    code: function( tsb ) {}
+    code: function( bee ) {}
   };
 
-  project.tasks[ "compile" ] = {
+  project.targets[ "compile" ] = {
     description: "Compile main source files.",
     depends: [],
     internal: true,
-    code: function( tsb ) {
-      tsb.tsc({
+    code: function( bee ) {
+      bee.tsc({
         file: "src/*.ts",
         targetDir: "target"
       });
     }
   };
 
-  project.tasks[ "clean" ] = {
+  project.targets[ "clean" ] = {
     description: "Delete all artifactes which will be created by this project.",
     depends: [],
-    code: function( tsb ) {
-      tsb.rmdir({ dir: "target" });
+    code: function( bee ) {
+      bee.rmdir({ dir: "target" });
     }
   }
 
-  project.tasks[ "test" ] = {
+  project.targets[ "test" ] = {
     description: "Run unit tests which test this project.",
     depends: [ "compileTests" ],
-    code: function( tsb ) {
-      tsb.test({
+    code: function( bee ) {
+      bee.test({
         file: "target/tests/tests.js"
       })      
     }
   };
 
-  project.tasks[ "compileTests" ] = {
+  project.targets[ "compileTests" ] = {
     description: "Compile unit tests.",
     depends: [ "compile" ],
     internal: true,
-    code: function( tsb ) {
-      tsb.tsc({
+    code: function( bee ) {
+      bee.tsc({
         file: "src/tests/tests.ts",
         targetDir: "target"
       })      
     }
   };
 
-  project.tasks[ "samples" ] = {
+  project.targets[ "samples" ] = {
     description: "Run sample unit tests.",
     depends: [ "compileSamples" ],
-    code: function( tsb ) {
-      tsb.node({
+    code: function( bee ) {
+      bee.node({
         file: "target/samples/sampletest.js"
       })      
-      tsb.node({
+      bee.node({
         file: "target/samples/tetris/tetristest.js"
       })      
     }
   }
 
-  project.tasks[ "compileSamples" ] = {
+  project.targets[ "compileSamples" ] = {
     description: "Compile sample unit tests.",
     depends: [ "compile" ],
     internal: true,
-    code: function( tsb ) {
-      tsb.tsc({
+    code: function( bee ) {
+      bee.tsc({
         file: "src/samples/*.ts src/samples/tetris/*.ts",
         targetDir: "target"
       })      
